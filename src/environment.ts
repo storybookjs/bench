7e1c3c908716d847cc78ed218480586168107bff
Michael Shilman <michael@lab80.co>
2020-09-10 20:33:01 +0800

    Handle no GCP credentials

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
-const GCP_CREDENTIALS = JSON.parse(process.env.GCP_CREDENTIALS || '');
+const GCP_CREDENTIALS = JSON.parse(process.env.GCP_CREDENTIALS || '{}');
 const SB_BENCH_UPLOAD = process.env.SB_BENCH_UPLOAD === 'true';
 const { CIRCLE_BRANCH, CIRCLE_SHA1 } = process.env;
 
