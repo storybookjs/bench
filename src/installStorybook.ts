40267015422c1f004bde9d3e27baf0bb7e289639
Michael Shilman <michael@lab80.co>
2020-11-10 00:40:23 +0800

    Don't install anything in no install command is provided

--- src/installStorybook.ts
+++ src/installStorybook.ts
@@ -8,15 +8,18 @@ const STDIO = 'inherit';
 
 export const installStorybook = async (installCommand: string) => {
   console.log('measuring install');
+  if (!installCommand) {
+    console.warn('No install command provided');
+    return {
+      size: { total: 0 },
+      time: { total: 0 },
+    };
+  }
 
   const initialSize = await du(NODE_MODULES);
   Tick.wrap(function install(done: () => void) {
-    if (installCommand) {
-      const [cmd, ...args] = installCommand.split(' ');
-      spawnSync(cmd, args, { stdio: STDIO });
-    } else {
-      console.warn('No install command provided');
-    }
+    const [cmd, ...args] = installCommand.split(' ');
+    spawnSync(cmd, args, { stdio: STDIO });
     done();
   });
   const finalSize = await du(NODE_MODULES);
